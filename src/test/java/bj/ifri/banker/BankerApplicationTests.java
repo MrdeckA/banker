9b123d5852c6aab60885fc196f35e3855902281f
Mrdeck <mrdeck30@gmail.com>
2024-01-06 14:38:10 +0100

    initial commit

---
+++ src/test/java/bj/ifri/banker/BankerApplicationTests.java
@@ -0,0 +1,13 @@
+package bj.ifri.banker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
